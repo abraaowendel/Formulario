e294275f677c42eb231d14c4cc0d9c40b922908e
Abraao Wendel <abraao.dev@gmail.com>
2022-04-05 10:30:45 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -3,26 +3,27 @@ const validator = {
         event.preventDefault();
         let send = true;
         const inputs = document.querySelectorAll('input');
+        validator.clearErrors();
+
         inputs.forEach((item) =>{
             let check = validator.checkInput(item);
             if(check !== true){
                 send = false;
-                alert(check)
+                validator.showError(item,check);
             }
         })
 
         if(send){
             form.submit();
         }
-    },
+    }, 
     checkInput: (input) =>{
         let rules = input.getAttribute('data-rules'); //PEGA ATRIBUTO DO INPUT
-        
-        if(rules != null){
+        if(rules != null){ 
             rules = rules.split('|') // SEPARA CONTEUDO DO ATRIBUTO CASO AJA
+
             for(let k in rules){
                 let details = rules[k].split('=') // SEPARA TIPO DA CONDIÇAO E A CONDIÇAO
-                
                 switch(details[0]){
                     case 'required':
                         if(input.value === ''){
@@ -37,9 +38,21 @@ const validator = {
                 }
             }
         };
-
        return true
-    }  
+    },
+    showError: (input,error) =>{
+        input.style.borderColor = '#FF0000';
+        let errorElement = document.createElement('div')
+        errorElement.classList.add('error')
+        errorElement.innerHTML = error;
+        input.parentElement.insertBefore(errorElement,input.ElementSibling)
+    },
+    clearErrors: () =>{
+        let errorElements = document.querySelectorAll('.error')
+        errorElements.forEach((erroItem) =>{
+            erroItem.remove()
+        })
+    }
 };
 
 let form = document.querySelector('.box')
